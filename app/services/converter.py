bca97ee18e7e1d27199ce0587c7e3f93413e3ab5
AlSalah00 <abdullah.aslah691@gmail.com>
2025-09-05 14:53:04 +0300

    Update converter.py

--- app/services/converter.py
+++ app/services/converter.py
@@ -1,26 +1,34 @@
 import subprocess
 from pathlib import Path
-from app.utils.file_manager import update_job_status
+from fastapi import HTTPException
+from app.utils.file_manager import scan_file, update_job_status
 
 
-def run_ffmpeg(input_path: Path, output_path: Path, job_id: str):
+def process_job(input_path: Path, output_path: Path, job_id: str):
     """
-    Convert media using FFmpeg.
+    Run antivirus scan and FFmpeg in sequence.
     """
     try:
+        # Scan file
+        scan_file(input_path)
+
+        # Run FFmpeg
         command = [
             "ffmpeg", "-y",
             "-i", str(input_path),
             str(output_path),
         ]
-
         subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
         update_job_status(job_id, "done")
 
     except subprocess.CalledProcessError as e:
-        update_job_status(job_id, "failed")
+        update_job_status(job_id, "failed", "ffmpeg conversion error.")
         print(f"[ERROR] FFmpeg failed for job {job_id}: {e.stderr.decode() if e.stderr else e}")
 
+    except HTTPException as e:
+        update_job_status(job_id, "failed", e.detail)    
+
     except Exception as e:
-        update_job_status(job_id, "failed")
+        update_job_status(job_id, "failed", str(e))
         print(f"[ERROR] Unexpected error for job {job_id}: {e}")
